35630792f739e15565701bfc1f009bfd65294a7b
neggoGH <neggo1985@mail.ru>
2021-01-07 09:25:55 +0300

    first method in Vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,4 +2,8 @@ public class Vehicle {
     int passengers;
     int fuelcap;
     int mpg;
+
+    void range() {
+        System.out.println("Дальность - " + fuelcap * mpg + " миль.");
+    }
 }
--- src/VehicleDemo.java
+++ src/VehicleDemo.java
@@ -1,17 +1,20 @@
 public class VehicleDemo {
     public static void main(String[] args) {
-       Vehicle car1 = new Vehicle();
-       Vehicle car2 = new Vehicle();
-       car2 = car1;
-       car1.mpg = 26;
-        System.out.println(car1.mpg);
-        System.out.println(car2.mpg);
-        Vehicle car3 = new Vehicle();
-        car2 = car3;
-        car3.mpg = 30;
-        System.out.println("_____________");
-        System.out.println(car1.mpg);
-        System.out.println(car2.mpg);
-        System.out.println(car3.mpg);
+       Vehicle minivan = new Vehicle();
+       Vehicle sportcar = new Vehicle();
+       int range1, range2;
+
+       minivan.passengers = 7;
+       minivan.fuelcap = 16;
+       minivan.mpg = 21;
+
+       sportcar.passengers = 2;
+       sportcar.fuelcap = 14;
+       sportcar.mpg = 12;
+
+        System.out.println("Мини-фургон может веревезти " + minivan.passengers + " пассажиров.");
+        minivan.range();
+        System.out.println("Спорткар может веревезти " + sportcar.passengers + " пассажиров.");
+        sportcar.range();
     }
 }
