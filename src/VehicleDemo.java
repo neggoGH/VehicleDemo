a899fee5f4ed554ead9c65a535f603b6da028735
neggoGH <neggo1985@mail.ru>
2021-01-07 10:59:58 +0300

    add param method fuelneeded

--- src/VehicleDemo.java
+++ src/VehicleDemo.java
@@ -2,6 +2,7 @@ public class VehicleDemo {
     public static void main(String[] args) {
        Vehicle minivan = new Vehicle();
        Vehicle sportcar = new Vehicle();
+       int dist = 252;
 
        minivan.passengers = 7;
        minivan.fuelcap = 16;
@@ -11,7 +12,9 @@ public class VehicleDemo {
        sportcar.fuelcap = 14;
        sportcar.mpg = 12;
 
-        System.out.println("Мини-фургон может веревезти " + minivan.passengers + " пассажиров на расстояние " + minivan.range() + " миль.");
-        System.out.println("Спорткар может веревезти " + sportcar.passengers + " пассажиров на расстояние " + sportcar.range() + " миль.");
+        System.out.println("Мини-фургон может веревезти " + minivan.passengers + " пассажиров на расстояние " + minivan.range() + " миль. " +
+                "Для преодоления "  + dist + " миль минифургону требуется " + minivan.fuelneeded(dist) + " галлонов топлива");
+        System.out.println("Спорткар может веревезти " + sportcar.passengers + " пассажиров на расстояние " + sportcar.range() + " миль." +
+                "Для преодоления "  + dist + " миль спорткару требуется " + sportcar.fuelneeded(dist) + " галлонов топлива");
     }
 }
